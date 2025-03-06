834f7d9d566a37e6763e88c660aedbe0ff6c93c3
carloselena <carloselenadev@gmail.com>
2025-03-06 12:05:12 -0400

    pivote file

--- lib/presentation/screens/screens.dart
+++ lib/presentation/screens/screens.dart
@@ -1,7 +1,7 @@
 export 'package:couteau/presentation/screens/about_me/about_me_screen.dart';
 export 'package:couteau/presentation/screens/age/age_screen.dart';
 export 'package:couteau/presentation/screens/country_universities/country_universities_screen.dart';
-export 'package:couteau/presentation/screens/genre/genre_screen.dart';
+export 'package:couteau/presentation/screens/gender/gender_screen.dart';
 export 'package:couteau/presentation/screens/home/home_screen.dart';
 export 'package:couteau/presentation/screens/pokemon/pokemon_screen.dart';
 export 'package:couteau/presentation/screens/weather/weather_screen.dart';
